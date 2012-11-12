a0a05342811a11bc4dc813b737a1153efa97d5f0
Andreas Gohr <gohr@cosmocode.de>
2012-11-12 15:30:53 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Creates a tar ball and leaves out some stuff not needed for a live
+# setup.
+
+gitexcludes='';
+for X in `cat .gitignore`
+do
+    gitexcludes="$gitexcludes --exclude=$X";
+done
+
+tar -czvf ideas.tgz -C .. \
+    --exclude=ideas.tgz \
+    --exclude=.git \
+    --exclude=.gitignore \
+    --exclude=.gitmodules \
+    --exclude=third_party/lessphp/tests \
+    --exclude=third_party/Twig/test \
+    --exclude=third_party/Twig/doc \
+    --exclude=sql/database.mwb \
+    $gitexcludes \
+    ideas
